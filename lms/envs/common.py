9b0de87e8b808e533bdc393427dc18c29d2e87aa
Ned Batchelder <ned@edx.org>
2015-06-17 18:10:28 -0400

    A real djangoapp to hold the theming code

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1797,6 +1797,9 @@ INSTALLED_APPS = (
     'staticfiles',
     'static_replace',
 
+    # Theming
+    'openedx.core.djangoapps.theming',
+
     # Our courseware
     'circuit',
     'courseware',
